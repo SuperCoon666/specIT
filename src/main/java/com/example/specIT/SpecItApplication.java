d3d830e39c4b4d23d89a3090f7a8e5033a72fd76
SuperCoon666 <voprishko03@gmail.com>
2023-03-24 11:31:29 +0300

    added library

---
+++ src/main/java/com/example/specIT/SpecItApplication.java
@@ -0,0 +1,13 @@
+package com.example.specIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/specIT/SpecItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.specIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
